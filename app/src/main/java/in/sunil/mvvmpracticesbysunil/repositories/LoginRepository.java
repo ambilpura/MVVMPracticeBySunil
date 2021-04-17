664b2c8366e5444159bce06516abf010230e8a5c
Sunil <ambilpura.sunil@gmail.com>
2021-04-17 15:36:49 +0530

    getting class from branch two

--- app/src/main/java/in/sunil/mvvmpracticesbysunil/repositories/LoginRepository.java
+++ app/src/main/java/in/sunil/mvvmpracticesbysunil/repositories/LoginRepository.java
@@ -22,6 +22,7 @@ public class LoginRepository {
         loginResponseBeanLiveData = new MutableLiveData<>();
         //Testing the branches
         //Testing orignal branches
+
         myApi = ApiUtils.getUserService();
 
         Call call = myApi.performLogin(userName, password);
